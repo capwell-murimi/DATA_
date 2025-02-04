eaddff90c03f314123b0a887936541b5ff31384b
capwell-murimi <capwellmurimi@gmail.com>
2025-02-04 19:21:17 +0300

    String methods

---
+++ JAVA/Stringmethod.java
@@ -0,0 +1,26 @@
+public class Stringmethod {
+    public static void main(String[] args) {
+        String name = "Capwell       ";
+        //boolean result = name.equals("Capwell");
+
+        //int result = name.length();
+
+        //char result = name.charAt(0);
+
+        //int result = name.indexOf("w");
+
+        //boolean result = name.isEmpty();
+
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+
+        //String result = name.trim();
+
+        //String result = name.replace("C","k");
+        //String result = name.substring(2, 6);
+
+        String result = name.substring(2);
+
+        System.out.println(result);
+    }
+}
