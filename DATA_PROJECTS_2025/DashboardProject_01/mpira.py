c8b5d6e94856dcc95a0ec26915e81afc04909277
capwell-murimi <capwellmurimi@gmail.com>
2025-02-18 18:14:18 +0300

    Start of my second analysis project

--- DATA_PROJECTS_2025/DashboardProject_01/mpira.py
+++ DATA_PROJECTS_2025/DashboardProject_01/mpira.py
@@ -2,7 +2,7 @@ import pandas as dragonWarrior
 
 df = dragonWarrior.read_csv(r'C:\Users\CapwellTheNerd\Desktop\DATA_\DATA_PROJECTS_2025\DashboardProject_01\players_data-2024_2025.csv')
 
-df = df.groupby('Nation')['Player'].count()
+new_column = df.groupby('Nation')['Player'].count()
 
 #nations = df['Nation'].count()
 
